d6a93667c937f8e57919beea6bc4a0dad73541e7
Kajsia Anderson <kajsia.anderson@gmail.com>
2022-12-05 15:22:10 -0700

    updated dummy quote

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -2,8 +2,8 @@ import React from 'react'
 import QuoteList from '../components/quotes/QuoteList'
 
 const DUMMY_QUOTES = [
-    {id: 'q1', author: 'Max', text: 'Learning React is fun!'},
-    {id: 'q2', author: 'Maximilian', text: 'Learning React is great!'}
+  { id: 'q1', author: 'Michael Scott', text: 'You Miss 100% Of The Shots You Don\'t Take. - Wayne Gretzky' },
+  { id: 'q2', author: 'Maximilian', text: 'Learning React is great!' }
 ]
 
 const AllQuotes = () => {
