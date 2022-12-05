f72156f2ce1a30a5613c93a1929b8eaf861ad8f0
Kajsia Anderson <kajsia.anderson@gmail.com>
2022-12-05 16:53:30 -0700

    added Firebase database

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,14 +1,36 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import QuoteList from '../components/quotes/QuoteList'
+import useHttp from '../hooks/use-http'
+import { getAllQuotes } from '../lib/api'
+import LoadingSpinner from '../components/UI/LoadingSpinner'
+import NoQuotesFound from '../components/quotes/NoQuotesFound'
 
-const DUMMY_QUOTES = [
-  { id: 'q1', author: 'Michael Scott', text: 'You Miss 100% Of The Shots You Don\'t Take. - Wayne Gretzky' },
-  { id: 'q2', author: 'Maximilian', text: 'Learning React is great!' }
-]
 
 const AllQuotes = () => {
+  const { sendRequest, status, data: loadedQuotes, error } = useHttp(getAllQuotes, true)
+
+  useEffect(() => {
+    sendRequest()
+  }, [sendRequest])
+
+  if (status === 'pending'){
+    return (
+      <div className='centered'>
+        <LoadingSpinner />
+      </div>
+    )
+  }
+
+  if (error) {
+    return <p className='centered focused'>{error}</p>
+  }
+
+  if (status === 'completed' && (!loadedQuotes || loadedQuotes.length === 0)) {
+    return <NoQuotesFound />
+  }
+
   return (
-    <QuoteList quotes={DUMMY_QUOTES} />
+    <QuoteList quotes={loadedQuotes} />
   )
 }
 
--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,31 +1,50 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useEffect } from 'react'
 import { useParams, Route, Link, useRouteMatch } from 'react-router-dom'
 import Comments from '../components/comments/Comments'
 import HighlightedQuote from '../components/quotes/HighlightedQuote'
+import useHttp from '../hooks/use-http'
+import { getSingleQuote } from '../lib/api'
+import LoadingSpinner from '../components/UI/LoadingSpinner'
 
-const DUMMY_QUOTES = [
-    { id: 'q1', author: 'Michael Scott', text: 'You Miss 100% Of The Shots You Don\'t Take. - Wayne Gretzky' },
-    { id: 'q2', author: 'Maximilian', text: 'Learning React is great!' }
-]
+// 'Michael Scott' 'You Miss 100% Of The Shots You Don\'t Take. - Wayne Gretzky' },
+
 
 const QuoteDetail = () => {
     const match = useRouteMatch()
     // console.log(match)
     const params = useParams()
 
-    const quote = DUMMY_QUOTES.find(quote => quote.id === params.quoteId)
+    const { quoteId } = params
 
-    if (!quote) {
+    const { sendRequest, status, data: loadedQuote, error } = useHttp(getSingleQuote, true)
+
+    useEffect(() => {
+        sendRequest(quoteId)
+    }, [sendRequest, quoteId])
+
+    if (status === 'pending') {
+        return (
+            <div className='centered'>
+                <LoadingSpinner />
+            </div>
+        )
+    }
+
+    if (error) {
+        return <p className='centered focused'>{error}</p>
+    }
+
+    if (!loadedQuote.text) {
         return <p>No quote found!</p>
     }
 
     return (
         <Fragment>
-            <HighlightedQuote text={quote.text} author={quote.author} />
+            <HighlightedQuote text={loadedQuote.text} author={loadedQuote.author} />
             <Route path={match.path} exact>
-            <div className='centered'>
-                <Link className='btn--flat' to={`${match.url}/comments`}>Load Comments</Link>
-            </div>
+                <div className='centered'>
+                    <Link className='btn--flat' to={`${match.url}/comments`}>Load Comments</Link>
+                </div>
             </Route>
             <Route path={`${match.path}/comments`}>
                 <Comments />
